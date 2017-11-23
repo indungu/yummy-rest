06b75cc433db70eba93aa4964cc4170e809d8f3d
indungu <in.wainaina93@gmail.com>
2017-11-23 20:23:53 +0300

    [PATCH] .travis.yml db service config

--- instance/config.py
+++ instance/config.py
@@ -1,10 +1,10 @@
 """App intance configs"""
 # Flask App Config
 DEBUG = True
-SECRET_KEY = "my_h@lleLluyh@"
+SECRET_KEY = "\xe6.]`\x99\x07\x1ap\xff\xb7c\xf0\xea*\xba{"
 
 # SQLAlchemy Configs
-SQLALCHEMY_DATABASE_URI = 'postgres://su:password@localhost:5432/yummy_rest_db'
+SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/yummy_rest_db'
 
 # Flask-RESTPlus Configs
 SWAGGER_UI_DOC_EXPANSION = 'list'
