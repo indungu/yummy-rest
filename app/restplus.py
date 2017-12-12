1207a5ef1693cc1628de1b9bffb2a8748a8aee91
indungu <in.wainaina93@gmail.com>
2017-12-12 08:23:44 +0300

    [ADD] Authorization for resource privatization

--- app/restplus.py
+++ app/restplus.py
@@ -1,6 +1,16 @@
 """RESTPLus API init"""
 from flask_restplus import Api
 
+# Setup authorization
+authorization = {
+    'apiKey': {
+        'type': 'apiKey',
+        'in': 'header',
+        'name': 'Authorization'
+    }
+}
+
 API = Api( version='1.0', title="Yummy REST",
-    description="REST API implementation of the Yummy Recipes using Flask and PostgreSQL"
+    description="REST API implementation of the Yummy Recipes using Flask and PostgreSQL",
+    authorizations=authorization
 )
