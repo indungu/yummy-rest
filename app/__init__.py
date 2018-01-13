9dc16327fe3f0933a983abd29dd335e2c0425365
Isaac Ndung'u <isaac.ndungu@andela.com>
2018-01-13 11:23:22 +0300

    [FIX] Invalid 404 response format: Previously return format was HTML this changes it to JSON.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 """Main APP module"""
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 # Linting exception
@@ -15,6 +15,22 @@ db = SQLAlchemy()
 
 APP = Flask(__name__, instance_relative_config=True)
 APP.config.from_object(app_config['development'])
+
+# overide 404 error handler
+
+@APP.errorhandler(404)
+def resource_not_found(error):
+    """
+    This will be response returned if the user attempts to access
+    a non-existent resource or url.
+    """
+
+    response_payload = dict(
+        message="The requested URL was not found on the server. " + \
+                 "If you entered the URL manually please check your spelling and try again."
+    )
+    return make_response(jsonify(response_payload), 404)
+
 db.init_app(APP)
 
 # Import and add namespaces for the endpoints
