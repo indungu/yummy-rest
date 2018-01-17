e090cfb9f7f54bf3ed1c9aa16811c1c0a0139e83
Isaac Ndung'u <isaac.ndungu@andela.com>
2018-01-18 02:42:21 +0300

    [ADD] Procfile for Heroku deployment [FIX] requirements.txt size.

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -132,12 +132,12 @@ class AuthNSTestCase(BaseTestCase):
         with self.client:
             # Ensure user can't register with a password that is less than
             # 8 characters long
-            user_details = dict(
+            new_user_details = dict(
                 email="some@person.com",
                 username="some_user",
                 password="pass"
             )
-            response = register_user(self, user_data=user_details)
+            response = register_user(self, user_data=new_user_details)
             data = json.loads(response.data.decode())
             errors = data['errors']
             self.assertTrue(
